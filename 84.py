366d0a06cedb8c154bc3d1a720f4ee5f05ca2d24
Rahila074 <rahilamuhammed074@gmail.com>
2024-10-04 18:11:22 +0530

    Add files via upload

---
+++ 84.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array(42)
+b = np.array([1,2,3,4])
+c = np.array([[1,2,3],[1,2,3]])
+d = np.array([[[1,1,1],[1,2,3],[2,3,45,]]])
+
+print(np.ndim(a))
+print(np.ndim(b))
+print(np.ndim(c))
+print(np.ndim(d))
+
+
